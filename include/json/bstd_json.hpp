d6e6fd9cc40fef43728991e74223f3287a711a3c
benagin <smithbr95@gmail.com>
2018-10-13 03:43:45 -0700

    Fixing gitignore to not ignore my directories.

---
+++ include/json/bstd_json.hpp
@@ -0,0 +1,8 @@
+#ifndef BSTD_JSON_HPP_
+#define BSTD_JSON_HPP_
+
+// Contains all public header files within the json tool.
+#include "./src/error/error.hpp"
+#include "./src/error/context_error.hpp"
+
+#endif
