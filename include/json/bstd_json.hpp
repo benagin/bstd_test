4b68b5b9f1ff28989dce0a66006633d197863f44
benagin <smithbr95@gmail.com>
2018-10-18 20:02:53 -0700

    Fixing public json header includes.

--- include/json/bstd_json.hpp
+++ include/json/bstd_json.hpp
@@ -2,7 +2,6 @@
 #define BSTD_JSON_HPP_
 
 // Contains all public header files within the json tool.
-#include "./src/error/error.hpp"
-#include "./src/error/context_error.hpp"
+#include "../../src/json/json.hpp"
 
 #endif
