b9802f81e1ff2d0266e13a6c013edabbb6502f8a
benagin <smithbr95@gmail.com>
2018-11-07 01:42:40 -0800

    Implementing unit_test. This resulted in some documentation changes.

--- src/test/unit_test.cpp
+++ src/test/unit_test.cpp
@@ -6,9 +6,9 @@ namespace bstd::test {
 
 unit_test&
 unit_test::
-add_test(const std::function<const result()>& _test)  {
-  // TODO: implement.
-
+add_test(const std::string& _name, const std::function<const result()>& _test) {
+  m_tests.insert(std::pair<std::string, std::function<const result()>>(_name,
+        _test));
   return *this;
 }
 
@@ -16,7 +16,32 @@ add_test(const std::function<const result()>& _test)  {
 void
 unit_test::
 test() const {
-  // TODO: implement.
+  int passed = 0;
+
+  for(const auto& [name, test] : m_tests) {
+    const auto& result = test();
+
+    std::cout << name;
+
+    if(result.first) {
+      ++passed;
+      std::cout << " passed." << std::endl;
+    }
+    else
+      std::cout << " failed: " << result.second << std::endl;
+  }
+
+  const auto& total = m_tests.size();
+  if(passed == total)
+    std::cout << "All tests passed!" << std::endl;
+  else {
+    const double ratio = ((double) passed / (double) total) * 100;
+    const int percent = ratio;
+
+    std::stringstream output;
+    output << percent << "% (" << passed << "/" << total << ") of tests passed.";
+    std::cout << output.str() << std::endl;
+  }
 }
 
 
--- src/test/unit_test.hpp
+++ src/test/unit_test.hpp
@@ -1,9 +1,11 @@
 #ifndef UNIT_TEST_HPP
 #define UNIT_TEST_HPP
 
+#include <iostream>
 #include <functional>
 #include <string>
-#include <vector>
+#include <sstream>
+#include <map>
 
 #include "result.hpp"
 
@@ -23,16 +25,17 @@ class unit_test {
     // Note: test() can be called as a part of this chain.
     // Example:
     //  auto test = new uni_test();
-    //  test.add_test(foo).add_test(bar).add_test(foo_bar).test();
-    virtual unit_test& add_test(const std::function<const result()>& _test) final;
+    //  test.add_test("foo", foo).add_test("bar", bar
+    //      .add_test("foo_bar", foo_bar).test();
+    virtual unit_test& add_test(const std::string& _name,
+        const std::function<const result()>& _test) final;
 
+    // Run each function in m_tests and write results to standard output.
     virtual void test() const final;
 
   private:
 
-    // The set of test functions.
-    // These are functions that return a const bool and take no arguments.
-    std::vector<std::function<const result()>> m_test;
+    std::map<std::string, std::function<const result()>> m_tests;
 
 };
 
