f3227f5da93407605a376e11bd040eddfb9a4564
benagin <smithbr95@gmail.com>
2018-11-14 23:56:19 -0800

    Adding json test class.

--- test/json/test_json1.cpp
+++ test/json/test_json1.cpp
@@ -1,14 +1,16 @@
 #include "test_json1.hpp"
 
-const bstd::test::result
-test_json1::
-test1() const {
-  return bstd::test::result(true, "");
-}
+BSTD_TEST_MAIN(test_json1)
+
 
 test_json1::
 test_json1() {
   ADD_TEST(test_json1::test1);
 }
 
-BSTD_TEST_MAIN(test_json1);
+
+const bstd::test::result
+test_json1::
+test1() const {
+  return bstd::test::result(true, "");
+}
--- test/json/test_json1.hpp
+++ test/json/test_json1.hpp
@@ -2,17 +2,16 @@
 #define TEST_JSON1_HPP_
 
 #include <bstd_json.hpp>
+
 #include <bstd_test.hpp>
 
-class test_json1 : public bstd::test::unit_test {
+class test_json1 final : public bstd::test::test {
 
   public:
 
     test_json1();
 
     ~test_json1() {}
-
-  private:
 
     const bstd::test::result test1() const;
 
