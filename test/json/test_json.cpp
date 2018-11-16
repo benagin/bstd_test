4079d88a3d03497109f13547f4405fb2edb82b9f
benagin <smithbr95@gmail.com>
2018-11-15 23:36:17 -0800

    Adding some json unit tests.

--- test/json/test_json.cpp
+++ test/json/test_json.cpp
@@ -5,12 +5,52 @@ BSTD_TEST_MAIN(test_json)
 
 test_json::
 test_json() {
-  ADD_TEST(test_json::test1);
+  ADD_TEST(test_json::json_size);
+  ADD_TEST(test_json::json_assignment_operator);
+  ADD_TEST(test_json::json_equal_operator);
+  ADD_TEST(test_json::json_addition_operator);
 }
 
 
 const bstd::test::result
 test_json::
-test1() const {
-  return bstd::test::result(true, "");
+json_size() const {
+  const auto zero = m_empty_json.size();
+  const auto one = m_one_child.size();
+
+  return bstd::test::result(zero == 0 && one == 1, "json size failed");
+}
+
+
+const bstd::test::result
+test_json::
+json_assignment_operator() const {
+  auto copy = m_json1;
+
+  // Check member variables.
+  const bool success = copy.get_path() == m_json1.get_path()
+      && copy.get_children() == m_json1.get_children();
+
+  return bstd::test::result(success, "json assignment failed");
+}
+
+
+const bstd::test::result
+test_json::
+json_equal_operator() const {
+  const bool test1 = m_json1 == m_json1_copy;
+  const bool test2 = m_json1 != m_json2;
+
+  return bstd::test::result(test1 && test2,
+      "json equal operator failed");
+}
+
+
+const bstd::test::result
+test_json::
+json_addition_operator() const {
+  auto result = m_json1 + m_json2;
+
+  return bstd::test::result(result.size() == 2 && result  == m_json1and2,
+      "json addition operator failed");
 }
--- test/json/test_json.hpp
+++ test/json/test_json.hpp
@@ -1,9 +1,8 @@
 #ifndef TEST_JSON_HPP_
 #define TEST_JSON_HPP_
 
-#include <bstd_json.hpp>
-
 #include <bstd_test.hpp>
+#include <bstd_json.hpp>
 
 class test_json final : public bstd::test::test {
 
@@ -13,7 +12,23 @@ class test_json final : public bstd::test::test {
 
     ~test_json() {}
 
-    const bstd::test::result test1() const;
+    const bstd::test::result json_size() const;
+    const bstd::test::result json_assignment_operator() const;
+    const bstd::test::result json_equal_operator() const;
+    const bstd::test::result json_addition_operator() const;
+
+
+  private:
+
+    std::string m_json_string1{"{ \"name1\": \"value1\" }"};
+    std::string m_json_string2{"{ \"name2\": \"value2\" }"};
+
+    bstd::json::json m_empty_json{""};
+    bstd::json::json m_one_child{"{}"};
+    bstd::json::json m_json1{m_json_string1};
+    bstd::json::json m_json1_copy{m_json_string1};
+    bstd::json::json m_json2{m_json_string2};
+    bstd::json::json m_json1and2{m_json_string1 + m_json_string2};
 
 };
 
