d7250ca5b44b741a5b4f479d5582c297ae8f26f3
benagin <smithbr95@gmail.com>
2019-01-18 08:22:16 -0800

    Cleaning up example.

--- examples/json/json_example1.cpp
+++ examples/json/json_example1.cpp
@@ -1,14 +1,15 @@
 #include <bstd_json.hpp>
 
 int main() {
-  const auto j = bstd::json::parse("");
+  const auto empty = bstd::json::parse("");
+  std::cout << "Empty: " << std::endl << *empty << std::endl;
 
-  std::cout << *j << std::endl;
+  const auto ws = bstd::json::parse(" ");
+  std::cout << "Whitespace: " << std::endl << *ws << std::endl;
 
-  const auto o = bstd::json::parse("{ \"test\": \"test\" }");
+  const auto json_object = bstd::json::parse("   { \"test\": \"test\" }");
+  std::cout << "Json object: " << std::endl << *json_object << std::endl;
 
-  std::cout << *o << std::endl;
-
-  const auto o2 = bstd::json::parse("    a");
-
+  const auto json_array = bstd::json::parse("   [ 1, 2, 3 ]");
+  std::cout << "Json array: " << std::endl << *json_array << std::endl;
 }
