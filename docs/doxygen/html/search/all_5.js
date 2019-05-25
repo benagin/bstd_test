a3a94793960637d36b3f755985287e341b3cde4d
benagin <smithbr95@gmail.com>
2019-05-25 14:40:05 -0700

    Update documentation.

--- docs/doxygen/html/search/all_5.js
+++ docs/doxygen/html/search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['test1',['test1',['../classtester__example1.html#ac230824bffd69f4598dc78d899b40e46',1,'tester_example1']]],
-  ['test2',['test2',['../classtester__example1.html#a727071d5fefa0311ce132257a7006e1c',1,'tester_example1']]],
+  ['test1',['test1',['../classtester__example1.html#ac5670ca53db0586491297abccb37a9bd',1,'tester_example1']]],
+  ['test2',['test2',['../classtester__example1.html#a2a8a944eb2edb9559ed0c155066ad4d1',1,'tester_example1']]],
   ['test_5ffunction',['test_function',['../classbstd_1_1test_1_1unit__tester.html#a53a22f2f1e6bd066a001e3bcd07fea4b',1,'bstd::test::unit_tester']]],
   ['tester_5fexample1',['tester_example1',['../classtester__example1.html',1,'tester_example1'],['../classtester__example1.html#a4b0bb4dbcb6e964b1dd34d8f78a9df24',1,'tester_example1::tester_example1()']]],
   ['tester_5fexample1_2ecpp',['tester_example1.cpp',['../tester__example1_8cpp.html',1,'']]],
--- docs/doxygen/html/search/functions_2.js
+++ docs/doxygen/html/search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['test1',['test1',['../classtester__example1.html#ac230824bffd69f4598dc78d899b40e46',1,'tester_example1']]],
-  ['test2',['test2',['../classtester__example1.html#a727071d5fefa0311ce132257a7006e1c',1,'tester_example1']]],
+  ['test1',['test1',['../classtester__example1.html#ac5670ca53db0586491297abccb37a9bd',1,'tester_example1']]],
+  ['test2',['test2',['../classtester__example1.html#a2a8a944eb2edb9559ed0c155066ad4d1',1,'tester_example1']]],
   ['tester_5fexample1',['tester_example1',['../classtester__example1.html#a4b0bb4dbcb6e964b1dd34d8f78a9df24',1,'tester_example1']]]
 ];
