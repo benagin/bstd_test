c26fa329bc2d6e03dac37a357035c99b6453d7e6
benagin <smithbr95@gmail.com>
2019-05-25 15:16:11 -0700

    Update documentation.

--- docs/doxygen/html/search/all_1.js
+++ docs/doxygen/html/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['bstd',['bstd',['../namespacebstd.html',1,'']]],
-  ['bstd_5funit_5ftest_5fmain',['BSTD_UNIT_TEST_MAIN',['../main_8hpp.html#a362fd4dc6b2992031b59fb7d3c178e97',1,'main.hpp']]],
+  ['bstd_5ftest_5fmain',['BSTD_TEST_MAIN',['../main_8hpp.html#a09525098ff3307bd17817fa130d45cc7',1,'main.hpp']]],
   ['test',['test',['../namespacebstd_1_1test.html',1,'bstd']]]
 ];
