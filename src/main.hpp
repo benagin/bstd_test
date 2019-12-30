aa51d4580c8208668730355a2a6a60a3722054dc
benagin <smithbr95@gmail.com>
2019-12-30 12:37:25 -0700

    Add explicit return statement in main. Add note for namespaces.

--- src/main.hpp
+++ src/main.hpp
@@ -3,11 +3,13 @@
 
 /// This includes a main function which runs the tests in the class provided as
 /// an argument.
+/// Note: do not include this define within a namespace.
 #define BSTD_TEST_MAIN(unit_tester) \
   int                               \
   main() {                          \
     unit_tester tester;             \
     tester.run();                   \
-  }
+    return 0;                       \
+  }                                 \
 
 #endif
